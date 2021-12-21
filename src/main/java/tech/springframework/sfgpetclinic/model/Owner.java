e8793189b43c58c94b009dfe30c16dda512d23b0
Sujith Surendran <sujithes@hotmail.com>
2021-12-21 16:53:23 +0530

    Add POJO class. Closes Issue #2 (patch)

---
+++ src/main/java/tech/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package tech.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/tech/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package tech.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
