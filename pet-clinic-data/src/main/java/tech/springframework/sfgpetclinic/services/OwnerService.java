68f963a5333c0d1db3dcbc222af2309a5df834aa
Sujith Surendran <sujithes@hotmail.com>
2021-12-21 20:17:14 +0530

    Create Interfaces for Pet Clinic Services. Closes #5

---
+++ pet-clinic-data/src/main/java/tech/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package tech.springframework.sfgpetclinic.services;
+
+import tech.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/tech/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package tech.springframework.sfgpetclinic.services;
+
+import tech.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
