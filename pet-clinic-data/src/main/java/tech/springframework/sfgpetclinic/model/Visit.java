10f3b4085c838ffa0321cfad130088c3959f2707
Sujith Surendran <sujithes@hotmail.com>
2022-01-02 00:30:22 +0530

    Create Vet Speciality Entity, Associate To Vet. Closes #27

--- pet-clinic-data/src/main/java/tech/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/tech/springframework/sfgpetclinic/model/Visit.java
@@ -3,7 +3,7 @@ package tech.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Visit extends BaseEntity {
-    
+
     private LocalDate date;
     private String description;
     private Pet pet;
